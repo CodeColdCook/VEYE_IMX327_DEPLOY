60d3bde4f8d225fcdad93320b07f121702b26d8e
周文龙 <867839609@qq.com>
2023-02-08 10:58:48 +0800

    Initial commit, 创建并测试jetpack5上的部署与测试

--- check_deploy.sh
+++ check_deploy.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-TARGET_DRIVER="veye327"
-TARGET_DEVICE="veye327"
+TARGET_DRIVER="veyecam"
+TARGET_DEVICE="veyecam"
 
 # check image and driver 
 echo "Check Image update status..."
-DRIVERS=`ls /sys/bus/i2c/drivers`
+DRIVERS=` ls /sys/bus/i2c/drivers `
 result=$(echo $DRIVERS | grep "${TARGET_DRIVER}")
 if [ "$result" != "" ]
 then
-    echo "- - - Image and Image.sig update success"
+    echo "- - - camera moudle deploy success"
 else
-    echo "- - - Image and Image.sig update failed"
-    echo "      no any veye327 found in /sys/bus/i2c/drivers"
+    echo "- - - camera moudle deploy update failed"
+    echo "      no any ${TARGET_DRIVER} found in /sys/bus/i2c/drivers"
 fi
 
 # check dtb and device
 echo "Check DTB update status..."
-DEVICES=`ls /proc/device-tree/cam_i2cmux/i2c@*`
+DEVICES=` ls /proc/device-tree/cam_i2cmux/i2c@* `
 result=$(echo $DEVICES | grep "${TARGET_DEVICE}")
 if [ "$result" != "" ]
 then
     echo "- - - DTB update success"
 else
     echo "- - - DTB update failed"
-    echo "      no any veye327 found in /proc/device-tree/cam_i2cmux/i2c@*"
+    echo "      no any ${TARGET_DEVICE} found in /proc/device-tree/cam_i2cmux/i2c@*"
 fi
 
 # for use
 echo "Use below commond for checking the camera vedio capture ↓↓↓"
-echo "  gst-launch-1.0 nvv4l2camerasrc device=/dev/video0 ! \"video/x-raw(memory:NVMM),format=(string)UYVY, width=(int)1920, height=(int)1080\" ! nvvidconv ! \"video/x-raw(memory:NVMM),format=(string)I420\" ! nvoverlaysink sync=false"
+echo "  gst-launch-1.0 nvv4l2camerasrc device=/dev/video0 ! "video/x-raw(memory:NVMM),format=(string)UYVY, width=(int)1920, height=(int)1080" ! nvvidconv ! "video/x-raw(memory:NVMM),format=(string)I420" ! nv3dsink sync=false"
