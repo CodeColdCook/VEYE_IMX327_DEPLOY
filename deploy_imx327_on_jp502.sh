54034c6ee3daa59baf80869cbf4d9eb9a2da4ba7
周文龙 <867839609@qq.com>
2023-02-08 10:58:48 +0800

    feat:创建并测试jetpack5上的部署与测试

---
+++ deploy_imx327_on_jp502.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SOURCE_FOLD="IMX327s-jetson-nx-502-asset"
+SOURCE_PATH=${HOME}/${SOURCE_FOLD}
+cd ${SOURCE_PATH}
+
+# install modules
+echo "*** deploy camera moudle *** "
+sudo cp -f 5.10.104-tegra/* /lib/modules/5.10.104-tegra/kernel/drivers/media/i2c/
+sudo depmod 
+echo "*** deploy camera moudle successful *** "
+# update dtb_file
+echo "*** update dtb_file *** "
+sudo cp -rf VEYE-MIPI-CAM2M/ /boot/veyecam
+echo "*** update dtb_file successful *** "
+
+# back up extlinux.conf
+echo "*** update extlinux.conf *** "
+sudo cp -f /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.bak
+# update extlinux.conf
+sudo cp -f ./extlinux_jp502_imx327.conf /boot/extlinux/extlinux.conf
+echo "*** update extlinux.conf successful *** "
+
+echo "*** please reboot and check *** "
